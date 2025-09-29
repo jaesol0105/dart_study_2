712241089d26bafb070ad8757ffdc75e2bb108c0
unknown <musou6413@naver.com>
2025-09-29 12:40:24 +0900

    students_grade 모듈 메서드 구성

---
+++ lib/students_grade.dart
@@ -0,0 +1,13 @@
+void getStudentsFileData() {}
+void getStudentStatus() {}
+void wirteResultsToFile() {}
+void getTopStudents() {}
+void getAverageAndMedian() {}
+void getGradeDistribution() {}
+void printMenu() {
+  print("메뉴를 선택하세요 :");
+  print("1. 특정 학생의 통계 확인");
+  print("2. 우수생 출력");
+  print("3. 전체 평균 점수 / 중앙값 출력");
+  print("4. 성적 분포표 출력");
+}
---
+++ test/dart_hw_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_hw_3/dart_hw_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
